5da6ed82422e9545979427417c5825afe7d24d74
Niraj Kumar <niraj411k@gmail.com>
2026-03-25 22:12:08 +1300

    Update delete-member 1. Updated member delete end point 2. Update product delete end point

--- src/app/delete-member/route.ts
+++ src/app/delete-member/route.ts
@@ -5,13 +5,13 @@ const API_URL = "https://api.bookingmood.com/v1";
 export async function POST(request: NextRequest) {
   const { member_id, product_id } = await request.json();
 
-  await fetch(`${API_URL}/revoke-member?id=${member_id}`, {
-    method: "POST",
+  await fetch(`${API_URL}/members/${member_id}`, {
+    method: "DELETE",
     headers: { Authorization: `Bearer ${process.env.BOOKINGMOOD_API_KEY}` },
   });
 
-  await fetch(`${API_URL}/delete-product?id=${product_id}`, {
-    method: "POST",
+  await fetch(`${API_URL}/products/${product_id}`, {
+    method: "DELETE",
     headers: { Authorization: `Bearer ${process.env.BOOKINGMOOD_API_KEY}` },
   });
 
